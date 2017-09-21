9c9be8937d33141b724354c1c3007a850f6952b4
bradberger01 <bradberger@hotmail.com>
2017-09-21 17:33:48 -0400

    More work on building Classes

--- bankAccountProject/Account.cs
+++ bankAccountProject/Account.cs
@@ -11,6 +11,8 @@ namespace bankAccountProject
         private int accountNumber;
         private double balance;
         private string accountType;
+     
+
 
         public int AccountNumber
         {
@@ -19,23 +21,38 @@ namespace bankAccountProject
         }
         public double Balance
         {
-            get { return this.accountNumber; }
-            set { this.accountNumber = value; }
+            get { return this.balance; }
+            set { this.balance = value; }
         }
         public string AccountType
         {
             get { return this.accountType; }
             set { this.accountType = value; }
         }
+       
 
         public Account()
         {
 
         }
+        public Account(string accountType, int accountNumber, double balance)
+        {
+            this.accountType = accountType;
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
 
         public virtual void ViewBalance()
         {
-
+            Console.WriteLine(balance);
+        }
+        public virtual void Deposit()
+        {
+            Console.WriteLine("Which account would you like to make a deposit to?");
+        }
+        public virtual void Withdrawl()
+        {
+            Console.WriteLine("Which account would you like to make a withdrawl from?");
         }
 
     }
---
+++ bankAccountProject/Checking.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankAccountProject
+{
+    class Checking: Account
+    {
+        private double addAmount;
+        private double subtractAmount;
+
+        public double AddAmount
+        {
+            get { return this.addAmount; }
+            set { this.addAmount = value; }
+        }
+        public double SubtractAmount
+        {
+            get { return this.subtractAmount; }
+            set { this.subtractAmount = value; }
+        }
+        public Checking()
+        {
+
+        }
+        public Checking(int accountNumber, string accountType, double balance)
+        {
+            this.AccountType = accountType;
+            this.AccountNumber = accountNumber;
+            this.Balance = balance;
+        }
+       // public virtual void Deposit(double balance, double addAmount)
+       // {
+       //   balance = balance + addAmount;
+       //     Console.WriteLine("You deposited $" + addAmount);
+       //     Console.WriteLine("Your new balance is $" + balance);
+       //}
+       // public override void Withdrawl(double balance, double subtractAmount)
+       // {
+       //     balance = balance - subtractAmount;
+       //     Console.WriteLine("You withdrew $" + subtractAmount);
+       //     Console.WriteLine("Your new balance is $" + subtractAmount);
+       // }
+    
+}
